e9b82b2862cd94a8b55a76e5355055516b460c90
Trevor Manz <trevmanz94@gmail.com>
2019-11-24 08:52:07 -0600

    use Result::unwrap instead

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,7 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    let result = std::fs::read_to_string(&args.path);
-    let content = match result {
-        Ok(content) => { content },
-        Err(error) => { panic!("Can't deal with {}, just exit here", error); }
-    };
+    let content = std::fs::read_to_string(&args.path).unwrap();
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
